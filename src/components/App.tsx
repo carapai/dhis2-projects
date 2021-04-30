04e3df04488da33abaa7515bbc9303e803cfa409
Charles Olupot <olupotcharles@gmail.com>
2021-04-30 08:45:32 +0300

    Just Home

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,13 @@
-const App = () => (
-  <div>
-    Home
-  </div>
-)
+import { Box } from '@chakra-ui/react'
+import { useD2 } from '../Context'
+const App = () => {
+  const d2 = useD2();
+  console.log(d2)
+
+  return (
+    <Box>
+      Home
+    </Box>
+  )
+}
 export default App
